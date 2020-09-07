d637d416a97782bf986fc8a69b6b9bb7e5e4e9c5
yosveni <yescalona@solinftec.com.br>
2020-09-07 20:50:17 -0300

    Updates

---
+++ precache-manifest.daecc3b97d6d95ee890db44d84f1b4f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5636ebdd47ec2a4df141ae2e584f0f70",
+    "url": "/challeger-deploy-app/index.html"
+  },
+  {
+    "revision": "46c036540b0b5e1b6b51",
+    "url": "/challeger-deploy-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "20364f65cd212099e9c2",
+    "url": "/challeger-deploy-app/static/css/main.3d7c48f5.chunk.css"
+  },
+  {
+    "revision": "46c036540b0b5e1b6b51",
+    "url": "/challeger-deploy-app/static/js/2.9525abcf.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "/challeger-deploy-app/static/js/2.9525abcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20364f65cd212099e9c2",
+    "url": "/challeger-deploy-app/static/js/main.578fcaa4.chunk.js"
+  },
+  {
+    "revision": "e4639bdd1e547748c04c",
+    "url": "/challeger-deploy-app/static/js/runtime-main.76341bce.js"
+  }
+]);
